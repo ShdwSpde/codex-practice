426e038ad88da8615f5b71e2b55e517c89fdbfbf
Mike Spade <mikespadeny@gmail.com>
2024-03-20 19:15:19 -0400

    Create initials.py

---
+++ python/initials.py
@@ -0,0 +1,10 @@
+# This is for Mike Geddes
+# Did you know I lived in the woods for 10 months?
+
+print('MMM   MMM GGGGGGGG')
+print('M  M M  M G      G')
+print('M  M M  M G       ')
+print('M  M M  M G    GGG')
+print('M   M   M G     G ')
+print('M   M   M G     G ')
+print('M   M   M GGGGGGGG')
