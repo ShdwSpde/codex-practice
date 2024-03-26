788af26fa24da3cd91468227349dff721f703a62
Mike Spade <mikespadeny@gmail.com>
2024-03-25 20:52:46 -0400

    Create hypotenuse.py

---
+++ python/hypotenuse.py
@@ -0,0 +1,5 @@
+# You can remember middle school. right?
+import math
+a = int(input('Enter the length of side a: '))
+b = int(input('Enter the length of side b: '))
+print(math.sqrt((a**2)+(b**2)))
