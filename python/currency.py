a89d36866369b4f9eef2783cc50886ebdee5c89d
Mike Spade <mikespadeny@gmail.com>
2024-03-25 21:03:09 -0400

    Create currency.py

---
+++ python/currency.py
@@ -0,0 +1,6 @@
+#I get money. Money I got
+pesos = int(input('What do you have left in pesos?'))
+soles = int(input('What do you have left in soles?'))
+reais = int(input('What do you have left in reais?'))
+print((pesos/16.68)+(soles/3.69)+(reais/5))
+
