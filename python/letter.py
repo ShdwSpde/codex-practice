c05b2f2003b2f4627493d5eed078efdd9023c66b
Mike Spade <mikespadeny@gmail.com>
2024-03-25 20:15:14 -0400

    Create letter.py

---
+++ python/letter.py
@@ -0,0 +1,6 @@
+# Day 2. Back at it with Python
+print('The date is 3/25/2024')
+print('Currently I feel excited to learn more about python')
+print('I want to get better at using sensors for environmental monitoring')
+print('Do not let them lowball you')
+print('🪴')
