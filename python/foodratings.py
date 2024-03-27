a6bcc4face357608550e6e658cfe4aff9d4b3b11
Mike Spade <mikespadeny@gmail.com>
2024-03-26 22:37:10 -0400

    Create foodratings.py

---
+++ python/foodratings.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+rating = float(input('How many stars did the restaurant review have?'))
+
+if rating > 4.5:
+  print('Extraordinary')
+elif rating > 4:
+  print('Excellent')
+elif rating > 3:
+  print('Good')
+elif rating > 2:
+  print('Fair')
+else:
+  print('Poor')
