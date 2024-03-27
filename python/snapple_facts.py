0169757aef11612ddd1649f29ba525f6837eb281
Mike Spade <mikespadeny@gmail.com>
2024-03-26 22:54:54 -0400

    Create snapple_facts.py

---
+++ python/snapple_facts.py
@@ -0,0 +1,17 @@
+# Write code below 💖
+import random
+
+snapple = random.randint(0,5)
+
+if snapple == 0: 
+  print('Flamingos turn pink from eating shrimp.')
+elif snapple == 1:
+  print("The only food that doesn't spoil is honey.")
+elif snapple == 2:
+  print('Shrimp can only swim backwards.')
+elif snapple == 3:
+  print("A taste bud's life span is about 10 days.")
+elif snapple == 4:
+  print('It is impossible to sneeze while sleeping.')
+else:
+  print('It is illegal to sing off-key in North Carolina.')
