4b7278ebad6e7c6f33b5c3536fde81ccab0cee5e
Mike Spade <mikespadeny@gmail.com>
2024-07-06 10:10:19 -0400

    Create met_pistols.py
    dictionaries with met art

---
+++ python/met_pistols.py
@@ -0,0 +1,16 @@
+#favorite art pieces at the met
+pistols = {
+  'title': 'Pair of Flintlock Pistols of Empress Catherine the Great',
+  'gunsmith': 'Johan Adolph Grecke',
+  'date': 1786,
+  'geography': 'Saint_Petersburg',
+  'culture': 'Russian',
+  'medium': ('steel','ivory','gold','brass'),
+  'classification': 'firearms'
+}
+
+print(pistols.keys())
+print(pistols.values())
+print(pistols.items())
+print(pistols)
+
