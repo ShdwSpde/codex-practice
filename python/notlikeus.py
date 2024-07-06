2f6b6bce6740a253a29a16d5a04bf34bffc31c7f
Mike Spade <mikespadeny@gmail.com>
2024-07-06 09:18:24 -0400

    Create notlikeus.py
    list practice

---
+++ python/notlikeus.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+genz_slang = ['bestie', 'tea', 'cap']
+
+for item in genz_slang:
+  print(item)
+print(genz_slang)
+
+dot_fuckemup = ['wop','wop','wop','wop','wop']
+for item in dot_fuckemup:
+  print(item)
+print(dot_fuckemup)
