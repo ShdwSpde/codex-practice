2fc50402ade51752d5832106ec9ec9132dc2bfe9
Mike Spade <mikespadeny@gmail.com>
2024-07-06 09:19:00 -0400

    Create friendsintuples.py
    tuples practice w location

---
+++ python/friendsintuples.py
@@ -0,0 +1,22 @@
+#start with listing your friends location
+Dev = (52.520008, 13.404954) #Berlin
+Pascale = (51.509865, -0.118092) #London
+Amauwa = (38.889805, -77.009056) #Washington DC
+Altaf = (6.80448, -58.15527) #Georgetown, Guyana
+Maggie = (39.742043, -104.991531) #Denver, CO
+Adrienne = (5.614818,-0.205874) #Accra, Ghana
+
+friends = (
+  Dev,
+  Pascale,
+  Amauwa,
+  Altaf,
+  Maggie,
+  Adrienne
+)
+
+print(Maggie)
+print(Altaf)
+
+for item in friends:
+  print(item)
